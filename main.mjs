8a7c9a51e18996003a6b2e768af98be6ad3eb896
Will Madden <w.a.madden@gmail.com>
2023-10-03 19:59:26 +0200

    Let's be classy and read Joyce instead

---
+++ main.mjs
@@ -0,0 +1,20 @@
+import { readFileSync } from "fs";
+import * as url from "url";
+
+const cwd = url.fileURLToPath(new URL(".", import.meta.url));
+
+const ulyssesLines = readFileSync(
+  `${cwd}/james-joyce-ulysses.txt`,
+  "utf-8"
+).split(/\r?\n/);
+const LOG_INTERVAL = 1000;
+
+function main() {
+  let i = 0;
+  setInterval(() => {
+    console.log(ulyssesLines[i % ulyssesLines.length]);
+    i += 1;
+  }, LOG_INTERVAL);
+}
+
+main();
